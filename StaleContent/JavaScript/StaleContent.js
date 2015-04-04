21cac608caf9efebac22ce2c5ac453f70d9b3c7d
sobek1985 <sobek1985@gmail.com>
2015-04-04 18:36:25 +0100

    Js to call entity service

--- StaleContent/JavaScript/StaleContent.js
+++ StaleContent/JavaScript/StaleContent.js
@@ -1,8 +1,36 @@
-﻿define(["sitecore"], function (Sitecore) {
-  var StaleContent = Sitecore.Definitions.App.extend({
-    initialized: function () {
+﻿require.config({
+    paths: {
+        entityService: "/sitecore/shell/client/Services/Assets/lib/entityservice"
     }
-  });
+});
 
-  return StaleContent;
+define(["sitecore", "entityService"], function (Sitecore, entityService) {
+    var StaleContent = Sitecore.Definitions.App.extend({
+        initialized: function () {
+            this.GetStaleContent();
+        },
+
+        EntityServiceConfig: function () {
+            var newsService = new entityService({
+                url: "/sitecore/api/ssc/StaleContent-Controllers/StaleContent"
+            });
+
+
+            return newsService;
+        },
+
+        GetStaleContent: function () {
+
+            var staleContentService = this.EntityServiceConfig();
+
+            var datasource = this.DataSource;
+
+            var result = staleContentService.fetchEntities().execute().then(function (stateContent) {
+                datasource.viewModel.items(stateContent);
+            });
+        },
+
+    });
+
+    return StaleContent;
 });
