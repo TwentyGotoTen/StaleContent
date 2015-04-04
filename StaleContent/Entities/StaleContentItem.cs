daa9d85200584ceb2e5da644e1305faf0fde7a17
sobek1985 <sobek1985@gmail.com>
2015-04-04 19:20:06 +0100

    Dummy content and databinding done in SPEAK

---
+++ StaleContent/Entities/StaleContentItem.cs
@@ -0,0 +1,8 @@
+﻿namespace StaleContent.Entities
+{
+    public class StaleContentItem : Sitecore.Services.Core.Model.EntityIdentity
+    {
+        public string itemId { get; set; }
+        public string Name { get; set; }
+    }
+}
--- StaleContent/Repositories/StaleContentRepository.cs
+++ StaleContent/Repositories/StaleContentRepository.cs
@@ -1,35 +1,41 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace StaleContent.Repositories
 {
-    public class StaleContentRepository : Sitecore.Services.Core.IRepository<Entities.StaleContent>
+    public class StaleContentRepository : Sitecore.Services.Core.IRepository<Entities.StaleContentItem>
     {
-        public IQueryable<Entities.StaleContent> GetAll()
+        public IQueryable<Entities.StaleContentItem> GetAll()
+        {
+            var items = new List<Entities.StaleContentItem>();
+
+            items.Add(new Entities .StaleContentItem(){Id = "1", itemId = "1", Name="Hello"});
+            items.Add(new Entities.StaleContentItem() { Id = "2", itemId = "2", Name = "world" });
+
+            return items.AsQueryable();
+        }
+
+        public Entities.StaleContentItem FindById(string id)
         {
             throw new System.NotImplementedException();
         }
 
-        public Entities.StaleContent FindById(string id)
+        public void Add(Entities.StaleContentItem entity)
         {
             throw new System.NotImplementedException();
         }
 
-        public void Add(Entities.StaleContent entity)
+        public bool Exists(Entities.StaleContentItem entity)
         {
             throw new System.NotImplementedException();
         }
 
-        public bool Exists(Entities.StaleContent entity)
+        public void Update(Entities.StaleContentItem entity)
         {
             throw new System.NotImplementedException();
         }
 
-        public void Update(Entities.StaleContent entity)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void Delete(Entities.StaleContent entity)
+        public void Delete(Entities.StaleContentItem entity)
         {
             throw new System.NotImplementedException();
         }
