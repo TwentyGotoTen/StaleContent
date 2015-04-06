246099f0abda1ce4e9a462209efcdccfda633099
Martin Davies <martin.davies@fusionworkshop.co.uk>
2015-04-06 11:06:35 +0100

    Merged initial SPEAK app work n to master with AutoMapper and StructureMap removed.

---
+++ StaleContent/Controllers/StaleContentController.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Services.Core;
+using Sitecore.Services.Infrastructure.Sitecore.Services;
+
+namespace StaleContent.Controllers
+{
+    [ServicesController]
+    public class StaleContentController : EntityService<Entities.StaleContentItem>
+    {
+        public StaleContentController(IRepository<Entities.StaleContentItem> repository) : base(repository)
+        {
+        }
+
+        //public StaleContentController() : this(Container.GetInstance<IRepository<Entities.StaleContentItem>>())
+        //{
+        //}
+    }
+}
---
+++ StaleContent/Repositories/StaleContentRepository.cs
@@ -0,0 +1,52 @@
+﻿using StaleContent.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StaleContent.Repositories
+{
+    public class StaleContentRepository : Sitecore.Services.Core.IRepository<Entities.StaleContentItem>
+    {
+        private DataAccess.IContentSearcher _iContentSearcher;
+
+        public StaleContentRepository(IContentSearcher iContentSearcher)
+        {
+            _iContentSearcher = iContentSearcher;
+        }
+
+
+        public IQueryable<Entities.StaleContentItem> GetAll()
+        {
+            var expiryPredicate = _iContentSearcher.CreateExpiryPredicate();
+            var searchResults = _iContentSearcher.GetItems(Constants.Indexes.Master, expiryPredicate);
+            throw new NotImplementedException();
+            //var staleContent = _iObjectMappers.MapListToListObject<Entities.SearchItem, Entities.StaleContentItem>(searchResults);
+            //return staleContent.AsQueryable();
+        }
+
+        public Entities.StaleContentItem FindById(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(Entities.StaleContentItem entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Exists(Entities.StaleContentItem entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Entities.StaleContentItem entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Entities.StaleContentItem entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
