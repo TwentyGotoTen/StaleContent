94e787adc929e2f36d0b8492599ad357dc04c75a
sobek1985 <sobek1985@gmail.com>
2015-04-06 17:26:24 +0100

    Temp JS

---
+++ StaleContent/JavaScript/StaleContent.js
@@ -0,0 +1,36 @@
+﻿require.config({
+    paths: {
+        entityService: "/sitecore/shell/client/Services/Assets/lib/entityservice"
+    }
+});
+
+define(["sitecore", "entityService"], function (Sitecore, entityService) {
+    var StaleContent = Sitecore.Definitions.App.extend({
+        initialized: function () {
+            // this.GetStaleContent();
+        },
+
+        EntityServiceConfig: function () {
+            var newsService = new entityService({
+                url: "/sitecore/api/ssc/StaleContent-Controllers/StaleContent"
+            });
+
+
+            return newsService;
+        },
+
+        GetStaleContent: function () {
+
+            var staleContentService = this.EntityServiceConfig();
+
+            var datasource = this.DataSource;
+
+            var result = staleContentService.fetchEntities().execute().then(function (stateContent) {
+                datasource.viewModel.items(stateContent);
+            });
+        },
+
+    });
+
+    return StaleContent;
+});
