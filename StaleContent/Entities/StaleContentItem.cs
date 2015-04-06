86033768ea432f4b74edc9e2d0926322c083bd44
Martin Davies <martin.davies@fusionworkshop.co.uk>
2015-04-06 16:57:58 +0100

    Added missing DataAccess, Entities, and references missing from previous cocked up merge

---
+++ StaleContent/Entities/StaleContentItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaleContent.Entities
+{
+    public class StaleContentItem : Sitecore.Services.Core.Model.EntityIdentity
+    {
+        public string itemId { get; set; }
+        public string Name { get; set; }
+
+        public DateTime Refreshed { get; set; }
+        public DateTime FreshnessExpiry { get; set; }
+    }
+}
