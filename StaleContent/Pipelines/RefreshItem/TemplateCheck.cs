4c2ed86f1c465b691ebfa8b72716775558594e68
sobek1985 <sobek1985@gmail.com>
2015-04-08 08:47:04 +0100

    Merge branch

--- StaleContent/Pipelines/RefreshItem/TemplateCheck.cs
+++ StaleContent/Pipelines/RefreshItem/TemplateCheck.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using StaleContent.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace StaleContent.Pipelines.RefreshItem
 {
-    public class TemplateCheck : RefreshItemProcessor 
+    public class TemplateCheck : RefreshItemProcessor
     {
         public override void Process(RefreshItemArgs args)
         {
-            // TODO
+            if (args == null || args.Item == null)
+                return;
+
+            if (!TemplateUtil.TemplateIsValid(args.Item))
+            {
+                args.AbortPipeline();
+            }
         }
     }
 }
