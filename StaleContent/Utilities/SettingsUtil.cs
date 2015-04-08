2bbac0a639974a48e0f0d8fc225df9b8cc1b9fd6
sobek1985 <sobek1985@gmail.com>
2015-04-08 08:47:04 +0100

    Merge branch

--- StaleContent/Utilities/SettingsUtil.cs
+++ StaleContent/Utilities/SettingsUtil.cs
@@ -13,7 +13,7 @@ namespace StaleContent.Utilities
         {
             get
             {
-                return Settings.GetSetting("StaleContent.GutterIconPath", "Business/32x32/calendar_warning.png");
+                return ConfigUtil.GetSetting("StaleContent.GutterIconPath", "Business/32x32/calendar_warning.png");
             }
         }
 
@@ -21,9 +21,13 @@ namespace StaleContent.Utilities
         {
             get
             {
-                int freshnessPeriod = Settings.GetIntSetting("StaleContent.FreshnessPeriod", 90);
-                Assert.IsTrue(freshnessPeriod > 0, "StaleContent.FreshnessPeriod setting must be a positive integer");
-                return freshnessPeriod;
+                string freshnessPeriod = ConfigUtil.GetSetting("StaleContent.FreshnessPeriod", "90");
+                int parsedFreshnessPeriod;
+                if (int.TryParse(freshnessPeriod, out parsedFreshnessPeriod))
+                {
+                    Assert.IsTrue(parsedFreshnessPeriod > 0, "StaleContent.FreshnessPeriod setting must be a positive integer");
+                }
+                return parsedFreshnessPeriod;
             }
         }
 
@@ -31,7 +35,7 @@ namespace StaleContent.Utilities
         {
             get
             {
-                return Settings.GetSetting("StaleContent.RefreshConfirmationDictionaryKey", "StaleContent.RefreshConfirmation");
+                return ConfigUtil.GetSetting("StaleContent.RefreshConfirmationDictionaryKey", "StaleContent.RefreshConfirmation");
             }
         }
 
@@ -39,8 +43,32 @@ namespace StaleContent.Utilities
         {
             get
             {
-                return Settings.GetSetting("StaleContent.IsStaleGutterTooltipDictionaryKey", "StaleContent.IsStaleGutterTooltip");
+                return ConfigUtil.GetSetting("StaleContent.IsStaleGutterTooltipDictionaryKey", "StaleContent.IsStaleGutterTooltip");
             }
-        }        
+        }
+
+        public static String ContentEditorWarningTitleDictionaryKey
+        {
+            get
+            {
+                return ConfigUtil.GetSetting("StaleContent.ContentEditorWarningTitleDictionaryKey", "StaleContent.ContentEditorWarningTitle");
+            }
+        }
+
+        public static String ContentEditorWarningTextDictionaryKey
+        {
+            get
+            {
+                return ConfigUtil.GetSetting("StaleContent.ContentEditorWarningTextDictionaryKey", "StaleContent.ContentEditorWarningText");
+            }
+        }
+
+        public static String ContentEditorWarningRefreshOptionDictionaryKey
+        {
+            get
+            {
+                return ConfigUtil.GetSetting("StaleContent.ContentEditorWarningRefreshOptionDictionaryKey", "StaleContent.ContentEditorRefreshOption");
+            }
+        }
     }
 }
